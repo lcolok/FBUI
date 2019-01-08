b06b1d360cd7a4de1e6169aca8e62fa587558433
lcolok <33664979+lcolok@users.noreply.github.com>
2019-01-08 10:26:24 +0800

    Update 上传文件与评论区同步.js

--- 上传文件与评论区同步.js
+++ 上传文件与评论区同步.js
@@ -68,9 +68,9 @@ async function getDiscussion(fileID) {
     headers: headers,
   })
   if (error) {
-    return console.log("Discussion请求出错: " + err);
+    return console.log("getDiscussion请求出错: " + err);
   }
-  console.log("Discussion请求成功: " + resp);
+  console.log("getDiscussion请求成功");
 
   var list = resp.data.data.list;
 
@@ -99,9 +99,9 @@ async function getAttachment(fileID) {
     headers: genericHeaders,
   })
   if (error) {
-    return console.log("Attachment请求出错: " + err);
+    return console.log("getAttachment请求出错: " + err);
   }
-  console.log("Attachment请求成功 ");
+  console.log("getAttachment请求成功 ");
 
 
 
@@ -144,10 +144,9 @@ async function postDiscussion(fileID, content) {
   })
   if (error) {
     return console.log("Discussion请求出错: " + err);
-  }
+  };
 
-  resp = JSON.parse(resp);
-  if (resp.code !== 0) {
+  if (resp.data.code !== 0) {
     console.log('讨论上传失败，错误信息：『' + resp.message + '』\n' + "详情请查看：" + "https://shimo.im/docs/" + fileID);
     return "error";
   } else {
@@ -183,29 +182,39 @@ async function googleTranslateByPost(orig) {
 
   var sl = 'auto';
   var tl = 'zh-CN';
-try{
-  var response = await axios({
-    method: 'POST',
-    url: "http://translate.google.cn/translate_a/single",
-    params: { "dt": "t", "q": orig, "tl": tl, "ie": "UTF-8", "sl": sl, "client": "ia", "dj": "1" }
-  });
+  try {
+    var response = await axios({
+      method: 'POST',
+      url: "http://translate.google.cn/translate_a/single",
+      params: { "dt": "t", "q": orig, "tl": tl, "ie": "UTF-8", "sl": sl, "client": "ia", "dj": "1" },
+      headers: {
+        "Accept": "*/*",
+        "Accept-Encoding": "br, gzip, deflate",
+        "Accept-Language": "zh-cn",
+        "Connection": "keep-alive",
+        "Cookie": "NID=154=Vf6msfWVov9Icm1WMYfq3dQ3BGHUlq6Txh5RHjnBtN48ZIZAdE_iQjxrrOMsOhbRlXXHtReYEm1rRKGUD3WsP1DhA0sO0nDf5S-J69qEBYRzIAY8nd1cE20wAKOr3cXxxPgwN12Dc5ly07v-F7RY-6Uv3ldkWGYeXWTgwkPR6Cs",
+        "Host": "translate.google.cn",
+        "User-Agent": "GoogleTranslate/5.26.59113 (iPhone; iOS 12.1; zh_CN; iPhone10,3)"
+      }
+    });
 
-  var i;
-  var output = '';
-  var trans = response.data.sentences;
-  if (trans.length > 1) {
-    for (i = 0; i < trans.length; ++i) {
-      output += trans[i]['trans'] + '\n';
+    var i;
+    var output = '';
+    var trans = response.data.sentences;
+    if (trans.length > 1) {
+      for (i = 0; i < trans.length; ++i) {
+        output += trans[i]['trans'] + '\n';
+      }
     }
+    else {
+      output = trans[0]['trans'];
+    }
+    console.log("谷歌翻译成功结果："+output);
+    return output;
+  } catch (e) {
+    console.log("谷歌翻译失败");
+    return orig;
   }
-  else {
-    output = trans[0]['trans'];
-  }
-  console.log(output);
-  return output;
-}catch(e){
-  return "none";
-}
 
 }
 
