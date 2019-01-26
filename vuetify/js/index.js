e43691dc7145047ccfdcb846a1469bde03f9f91f
lcolok <425311101@qq.com>
2019-01-27 04:20:02 +0800

    基本完成搜索框逻辑

--- vuetify/js/index.js
+++ vuetify/js/index.js
@@ -1,26 +1,46 @@
 new Vue({
   el: '#app',
   data: () => ({
-    dark: true,
+    dark: false,
+
     drawers: ['Default (no property)', 'Permanent', 'Temporary'],
     primaryDrawer: {
       model: null,
       type: 'default (no property)',
-      clipped: false,
+      clipped: true,
       floating: false,
       mini: false
     },
     footer: {
       inset: false
     },
-
+    searchLoading: false,
+    keyword: '',
+    custom: true,
     loader: null,
     loading: false,
     loading2: false,
     loading3: false,
     loading4: false,
-    alert: true
+    alert: true,
+    rules: {
+      required: value => !!value || '必填信息',
+      counter: value => (value == null ? 0 : value.length) <= 20 || '最多只能填写20个字符',
+      email: value => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'Invalid e-mail.'
+      }
+    }
   }),
+  computed: {
+    progress() {
+      var num = this.keyword == null ? 0 : this.keyword.length;
+      return Math.min(100, num * 10)
+    },
+    color() {
+      return ['error', 'warning', 'success'][Math.floor(this.progress / 40)]
+    }
+  },
   watch: {
     loader() {
       var _this = this;
@@ -31,5 +51,44 @@ new Vue({
 
       this.loader = null;
     }
-  }
+  },
+  methods: {
+    
+    
+    searchByKeyword(delay) {
+      var target = this;
+      if (target.keywordLasttime != target.keyword) {
+        if (target.lastTime == 0) {
+          searchLC(target, delay);
+        } else {
+          clearTimeout(target.lastTime);
+          searchLC(target, delay);
+        }
+      }
+    }
+
+  },
+
 })
+
+
+
+function searchLC(target, delay) {
+  target.lastTime = setTimeout(() => {
+    showLoading(target);
+    target.keywordLasttime = target.keyword;
+    window.location.href = `#/${target.keyword}`
+    console.log('关键词为:' + target.keyword);
+  }, delay)
+}
+
+
+function showLoading(target) {
+  target.custom = false;
+  target.searchLoading = true;
+  setTimeout(() => {
+    target.custom = true;
+    target.searchLoading = false;
+  }, 2000)
+}
+
